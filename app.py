cea8b2b2341750f53ef3641da9e483c9a11db39d
aswrise <aquaswang@gmail.com>
2023-02-23 10:05:01 -0500

    update test 1

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ def json_example():
     return jsonify(
         tracklist=list(set(recommend)),
         version=os.environ['VERSION'],
-        model_date= time
+        model_date= time,
+        are_youhappy="yes"
 
         # version=g.user.email,
         # model_date=g.user.id,
